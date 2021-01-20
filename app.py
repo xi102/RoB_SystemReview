7555f7e61c5bb7d3207de516a51e023793dffbbd
zhang.j.k <zhangjk67@gmail.com>
2021-01-20 11:35:28 +0800

    添加文章上传说明、CopyRight等

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ UPLOAD_FOLDER = 'data/Upload_pdf'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER  # 设置文件上传的目标文件夹
 basedir = os.path.abspath(os.path.dirname(__file__))  # 获取当前项目的绝对路径
 # ALLOWED_EXTENSIONS = set(['txt', 'png', 'jpg', 'xls', 'JPG', 'PNG', 'xlsx', 'gif', 'GIF','pdf'])  # 允许上传的文件后缀
-ALLOWED_EXTENSIONS = set(['pdf'])
-
+ALLOWED_EXTENSIONS = set(['pdf']) # 允许上传的文件后缀
 
 # 判断文件是否合法
 def allowed_file(filename):
@@ -24,24 +23,6 @@ def allowed_file(filename):
 def upload_test():
     return render_template('upload.html')
 
-
-@app.route('/uploading', methods=['POST'], strict_slashes=False)
-def api_upload():
-    file_dir = os.path.join(basedir, app.config['UPLOAD_FOLDER'])  # 拼接成合法文件夹地址
-    if not os.path.exists(file_dir):
-        os.makedirs(file_dir)  # 文件夹不存在就创建
-    f = request.files['myfile']  # 从表单的file字段获取文件，myfile为该表单的name值
-    if f and allowed_file(f.filename):  # 判断是否是允许上传的文件类型
-        fname = f.filename
-        ext = fname.rsplit('.', 1)[1]  # 获取文件后缀
-        unix_time = int(time.time())
-        new_filename = str(unix_time) + '.' + ext  # 修改文件名
-        f.save(os.path.join(file_dir, new_filename))  # 保存文件到upload目录
-        # return jsonify({"errno": 0, "errmsg": "上传成功"})
-        return redirect('/pre_result')
-    else:
-        # return jsonify({"errno": 1001, "errmsg": "上传失败"})
-        return "上传文件失败！！！"
 
 
 @app.route('/upload', methods=['POST'], strict_slashes=False)
@@ -67,18 +48,18 @@ def api_upload02():
 @app.route('/pre_result')
 def hello_world():
     #####（pdf转txt）
-    pdf_Base_path = "D:/Projects/PythonProjects/NLP_Projects/RoB_SystemReview/data/Upload_pdf"
-    txt_Base_Path = "D:/Projects/PythonProjects/NLP_Projects/RoB_SystemReview/data/Processed_txt"
+    pdf_Base_path = ".._V2.0/data/Upload_pdf"
+    txt_Base_Path = ".._V2.0/data/Processed_txt"
     pdfTotxt(pdf_Base_path, txt_Base_Path)
 
     # ######（txt文本处理）
-    inBasePath = 'D:/Projects/PythonProjects/NLP_Projects/RoB_SystemReview/data/Processed_txt'
-    outBasePath = 'D:/Projects/PythonProjects/NLP_Projects/RoB_SystemReview/data/Txt_pre'
+    inBasePath = '.._V2.0/data/Processed_txt'
+    outBasePath = '.._V2.0/data/Txt_pre'
     txtTo_Pre_Data(inBasePath, outBasePath)
 
     #######（文档预测）
-    txtFilePath = 'D:/Projects/PythonProjects/NLP_Projects/RoB_SystemReview/data/Txt_pre'
-    modelBasePath = 'D:/Projects/PythonProjects/NLP_Projects/RoB_SystemReview/data/model'
+    txtFilePath = '.._V2.0/data/Txt_pre'
+    modelBasePath = '.._V2.0/data/model'
     doc_pre_result = docPrediction(txtFilePath, modelBasePath)
     for key in doc_pre_result:
         print(key, ":", end=" ")
@@ -87,8 +68,8 @@ def hello_world():
         print()
 
     # #######（句子预测）
-    txtFilePath = 'D:/Projects/PythonProjects/NLP_Projects/RoB_SystemReview/data/Txt_pre'
-    modelBasePath = 'D:/Projects/PythonProjects/NLP_Projects/RoB_SystemReview/data/model'
+    txtFilePath = '.._V2.0/data/Txt_pre'
+    modelBasePath = '.._V2.0/data/model'
     sen_pre_result = senPrediction(txtFilePath, modelBasePath)
     for key in sen_pre_result:
         print(key, ":", end=" ")
